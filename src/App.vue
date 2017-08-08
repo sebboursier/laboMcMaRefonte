ef78ea7a0526b6190f614682a9cc089501641157
Sébastien Boursier <sebastien.boursier@mcma-solutions.com>
2017-08-08 18:32:17 +0200

    test
    qs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-
+  
     <header class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container-fluid">
-
+  
         <div class="navbar-header">
           <router-link to="/home" class="navbar-brand">Labo VueJs</router-link>
         </div>
-
+  
       </div>
     </header>
-
+  
+    <div class="coucu"></div>
+  
     <div class="content">
       <router-view></router-view>
     </div>
